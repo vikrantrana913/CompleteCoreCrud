410082142c09416ec9ca72c573e86b1e45d8468e
Vikrant Singh <vikrantrana913@gmail.com>
2025-09-29 03:26:46 +0530

    Adding some new file

---
+++ CoreProjectVikrantPractice1/Models/CandidateDBModel.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreProjectVikrantPractice1.Models
+{
+    public class CandidateDBModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="The field is required.")]
+        [Display(Name="Name")]
+        [MaxLength(50,ErrorMessage ="Maximum 50 charactor")]
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "The field is required.")]
+        public int Country { get; set; }
+
+        [Required(ErrorMessage = "The field is required.")]
+        public int State { get; set; }
+
+        [Required(ErrorMessage = "The field is required.")]
+        public int City { get; set; }
+
+        [Required(ErrorMessage = "Photo is required")]
+       
+        public string Photo { get; set; }
+
+        [Required(ErrorMessage = "The field is required.")]
+        public int Designation { get; set; }
+
+        public bool Python { get; set; }
+
+        public bool Java { get; set; }
+    }
+
+    public class Country
+    {
+        public int CountryId { get; set; }
+
+        public string CountryName { get; set; }
+
+    }
+
+    public class State
+    {
+        public int StateId { get; set; }
+
+        public string StateName { get; set; }
+
+        public int CountryId { get; set; }
+
+    }
+
+    public class City
+    {
+        public int CityId { get; set; }
+
+        public string CityName { get; set; }
+
+        public int StateId { get; set; }
+    }
+
+
+
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,100 @@
+Create Database Practice
+use Practice
+
+Create table Candidate
+(
+Id bigint Primary key identity(1,1),
+Name nvarchar(50),
+Gender nvarchar(10),
+Country bigint foreign key references Country(CountryId),
+State bigint foreign key references State(StateId),
+City bigint foreign key references City(CityId),
+Python bit,
+Java bit,
+Photo nvarchar(50),
+Desingnation bigint foreign key references Desingnation(JobId)
+)
+
+Create table Desingnation
+(
+JobId bigint primary key identity(1,1),
+Jobtitle nvarchar(50)
+)
+
+select * from Desingnation
+
+
+Create table Country
+(
+CountryId bigint Primary key identity(1,1),
+CountryName nvarchar(50)
+)
+
+Create table State
+(
+StateId bigint primary key identity(1,1),
+StateName nvarchar(50),
+CountId bigint foreign key references Country(CountryId)
+)
+
+Create table City
+(
+CityId bigint primary key identity(1,1),
+CityName nvarchar(50),
+StatId bigint foreign key references State(StateId)
+)
+
+Create Procedure AddCandidateData
+(
+@Name nvarchar(50),
+@Gender nvarchar(10),
+@Country bigint,
+@State bigint,
+@City bigint,
+@Python bit,
+@Java bit,
+@Photo nvarchar(50),
+@Desingnation bigint
+)
+as
+begin
+Insert into Candidate(Name,Gender,Country,State,City,Python,Java,Photo,Desingnation)
+Values(@Name,@Gender,@Country,@State,@City,@Python,@Java,@Photo,@Desingnation)
+End
+
+Select * from Candidate
+
+Create procedure GetCountryList
+as
+begin
+Select * from Country
+end
+
+Create procedure GetStateList
+(
+@CountId bigint
+)
+as
+begin
+Select * from State where CountId=@CountId
+end
+
+Create procedure GetCityList
+(
+@StatId bigint
+)
+as
+begin	
+select * from City where StatId=@StatId
+end
+
+Create procedure GetDesingnationList
+as
+begin
+select * from Desingnation
+end
+
+exec GetDesingnationList
+
+--- Saturday update --- 
+Select * from Candidate
